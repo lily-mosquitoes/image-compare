c98aab7e6fcbc456300a679268ed9b60e150951e
Lílian <lily.mosquitoes@gmail.com>
2022-12-13 01:19:00 +0200

    test(app): create tests for cookies

--- src/main.rs
+++ src/main.rs
@@ -36,29 +36,82 @@ pub mod tests {
     wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
     use super::*;
-    use gloo_utils;
-    use std::time::Duration;
     use wasm_bindgen_test::*;
+    use wasm_bindgen::JsCast;
+    use web_sys;
+    use std::time::Duration;
     use yew::platform::time::sleep;
 
+    fn document() -> web_sys::Document {
+        web_sys::window()
+            .expect("window to be present")
+            .document()
+            .expect("document to be present") 
+    }
+
     fn get_inner_html_by_id(id: &str) -> String {
-        gloo_utils::document()
+        document()
             .get_element_by_id(id)
             .expect("element with id to be present")
             .inner_html()
     }
 
+    fn htmldocument() -> web_sys::HtmlDocument {
+        document()
+            .dyn_into::<web_sys::HtmlDocument>()
+            .expect("Document to be castable to HtmlDocument")
+    }
+
+    fn get_cookies() -> Option<String> {
+        htmldocument()
+            .cookie()
+            .ok()
+    }
+
+    fn cookie_exists(name: &str) -> bool {
+        if let Some(cookies) = get_cookies() {
+            cookies.contains(name)
+        } else {
+            false
+        }
+    }
+
     #[wasm_bindgen_test]
-    async fn test_wasm() {
+    async fn test_cookies_absent() {
         yew::Renderer::<App>::with_root(
-            gloo_utils::document().get_element_by_id("output").unwrap(),
+            document().get_element_by_id("output").unwrap(),
         )
         .render();
 
         // wait for rendering
         sleep(Duration::from_millis(100)).await;
 
+        let _ = htmldocument()
+            .set_cookie("fingerprint=; \
+                        expires=Thu, 01 Jan 1970 00:00:00 UTC; \
+                        path=/;");
+
+        assert!(!cookie_exists("fingerprint"));
+
         let result = get_inner_html_by_id("main");
+        assert_eq!(result.as_str(), "<h1>Hello</h1>");
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_cookies_present() {
+        yew::Renderer::<App>::with_root(
+            document().get_element_by_id("output").unwrap(),
+        )
+        .render();
+
+        // wait for rendering
+        sleep(Duration::from_millis(100)).await;
+
+        let _ = htmldocument().set_cookie("fingerprint=testvalue");
+
+        assert!(cookie_exists("fingerprint"));
+
+        let result = get_inner_html_by_id("main");        
         assert_eq!(result.as_str(), "<h1>Hello</h1>");
     }
 }
