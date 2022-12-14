947b8aaf04c58de25eef342d8ed5c9f3c8800126
Lílian <lily.mosquitoes@gmail.com>
2022-12-14 16:49:31 +0200

    feat(welcome): implement get fingerprint button functionality

--- src/pages/welcome.rs
+++ src/pages/welcome.rs
@@ -1,11 +1,28 @@
 use yew::prelude::*;
 
 #[function_component(Welcome)]
-pub fn welcome() -> Html {   
+pub(crate) fn welcome() -> Html {
+    let get_fingerprint = Callback::from(move |_| {
+        use web_sys;
+        use wasm_bindgen::JsCast;
+        fn htmldocument() -> web_sys::HtmlDocument {
+            web_sys::window()
+                .expect("window to be present")
+                .document()
+                .expect("document to be present") 
+                .dyn_into::<web_sys::HtmlDocument>()
+                .expect("Document to be castable to HtmlDocument")
+        }
+
+        htmldocument()
+            .set_cookie("fingerprint=testvalue;path=/")
+            .unwrap();
+    });
+    
     html! {
         <section id="welcome">
             <h1>{ "No fingerprint" }</h1>
-            <button id="get_fingerprint">
+            <button id="get_fingerprint" onclick={get_fingerprint}>
                 { "Get fingerprint" }
             </button>
         </section>
