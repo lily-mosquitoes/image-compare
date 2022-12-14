1a442c6362b56167d2ca871de2a37ae943e23b53
Lílian <lily.mosquitoes@gmail.com>
2022-12-14 16:35:34 +0200

    test(welcome): create test for get fingerprint button

--- src/pages/welcome.rs
+++ src/pages/welcome.rs
@@ -4,8 +4,38 @@ use yew::prelude::*;
 pub fn welcome() -> Html {   
     html! {
         <section id="welcome">
-            <h1>{ "No cookies" }</h1>
+            <h1>{ "No fingerprint" }</h1>
+            <button id="get_fingerprint">
+                { "Get fingerprint" }
+            </button>
         </section>
     }
 }
 
+#[cfg(test)]
+pub(crate) mod tests {
+    use super::*;
+    use crate::tests_setup::*;
+
+    setup_environment!();
+
+    #[wasm_bindgen_test]
+    async fn test_get_fingerprint() {
+        WasmWindow::document()
+            .set_raw_cookies(EMPTY_FINGERPRINT);
+
+        render_app!(Welcome);
+
+        let fingerprint_button = WasmWindow::document()
+            .get_element_by_id("get_fingerprint")
+            .as_html();
+
+        fingerprint_button.click();
+
+        let cookies = WasmWindow::document()
+            .get_raw_cookies()
+            .expect("cookies to not be null");
+
+        assert!(cookies.contains("fingerprint"));
+    }
+}
