4279429f86ccc3d3fa703c6175f23ec8833e1752
Lílian <lily.mosquitoes@gmail.com>
2023-02-14 14:03:55 +0200

    feat(footer): make language button show current languange

--- src/shared_components/footer.rs
+++ src/shared_components/footer.rs
@@ -1,3 +1,5 @@
+use std::sync::atomic::Ordering;
+
 use yew::{
     classes,
     function_component,
@@ -7,7 +9,11 @@ use yew::{
     Properties,
 };
 
-use crate::shared_components::Button;
+use crate::{
+    shared_components::Button,
+    AVAILABLE_LANGUAGES,
+    SELECTED_LANGUAGE,
+};
 
 #[derive(Properties, PartialEq, Default)]
 pub(crate) struct FooterProps {
@@ -16,6 +22,22 @@ pub(crate) struct FooterProps {
 
 #[function_component(Footer)]
 pub(crate) fn footer(props: &FooterProps) -> Html {
+    let available_languages: Vec<String> = AVAILABLE_LANGUAGES
+        .iter()
+        .map(|x| format!("{}", x.display()))
+        .collect();
+
+    let index = SELECTED_LANGUAGE.load(Ordering::SeqCst);
+
+    let selected_language = match (
+        AVAILABLE_LANGUAGES.len() > 0,
+        AVAILABLE_LANGUAGES.len() > index,
+    ) {
+        (true, true) => available_languages[index].clone(),
+        (true, false) => available_languages[0].clone(),
+        (false, _) => "".to_string(),
+    };
+
     html! {
         <footer
             id="footer"
@@ -36,7 +58,7 @@ pub(crate) fn footer(props: &FooterProps) -> Html {
                     "border-gray-300",
                 ]}
             >
-                { "EN" }
+                { selected_language }
             </Button>
             {for props.children.iter()}
         </footer>
@@ -45,6 +67,8 @@ pub(crate) fn footer(props: &FooterProps) -> Html {
 
 #[cfg(test)]
 mod tests {
+    use std::sync::atomic::Ordering;
+
     use wasm_bindgen_test::{
         wasm_bindgen_test,
         wasm_bindgen_test_configure,
@@ -55,6 +79,8 @@ mod tests {
         dom::DOM,
         render_yew_component,
         wasm_sleep_in_ms,
+        AVAILABLE_LANGUAGES,
+        SELECTED_LANGUAGE,
     };
     wasm_bindgen_test_configure!(run_in_browser);
 
@@ -66,5 +92,34 @@ mod tests {
         assert!(
             DOM::get_button_by_id("select_language_button").is_some()
         );
+    }
+
+    #[wasm_bindgen_test]
+    async fn select_language_button_shows_selected_language() {
+        // add 1 to len to run even if no languages are available
+        for selected_language in 0..AVAILABLE_LANGUAGES.len() + 1 {
+            SELECTED_LANGUAGE
+                .store(selected_language, Ordering::SeqCst);
+
+            render_yew_component!(Footer);
+            wasm_sleep_in_ms(50).await;
+
+            let button =
+                DOM::get_button_by_id("select_language_button")
+                    .expect(
+                        "Element #select_language_button to exist",
+                    );
+
+            let index = selected_language;
+            let expected = if AVAILABLE_LANGUAGES.len() > index {
+                format!("{}", AVAILABLE_LANGUAGES[index].display())
+            } else if AVAILABLE_LANGUAGES.len() > 0 {
+                format!("{}", AVAILABLE_LANGUAGES[0].display())
+            } else {
+                "".to_string()
+            };
+
+            assert_eq!(button.inner_html(), expected);
+        }
     }
 }
