e7cfacfa4738badcc88f1673c697db1a58b2c796
Lílian Ferreira de Freitas <lily.mosquitoes@gmail.com>
2024-06-14 18:39:35 +0300

    refactor: use trunk built in support for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  content: ["./src/**/*.rs"],
+  content: ["./src/**/*.{rs,html}"],
   theme: {
     extend: {
       fontFamily: {
