19df9f55bf12ac3ff359d9f983a03ef5926205e2
Lílian Ferreira de Freitas <lily.mosquitoes@gmail.com>
2024-06-17 01:54:04 +0300

    refactor: change paths of ssl files
    The new paths work better with how letsencrypt sets up certificates.

--- .tls/generate.sh
+++ .tls/generate.sh
@@ -1,2 +1,3 @@
-openssl genpkey -genparam -algorithm ec -pkeyopt ec_paramgen_curve:P-256 -out ECPARAM.pem
-openssl req -x509 -nodes -newkey ec:ECPARAM.pem -keyout privkey.pem -out fullchain.pem -days 10 -config openssl.cnf
+mkdir -p live/localhost
+openssl genpkey -genparam -algorithm ec -pkeyopt ec_paramgen_curve:P-256 -out live/localhost/ECPARAM.pem
+openssl req -x509 -nodes -newkey ec:live/localhost/ECPARAM.pem -keyout live/localhost/privkey.pem -out live/localhost/fullchain.pem -days 10 -config openssl.cnf
